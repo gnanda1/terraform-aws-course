a7fe769d9a6ea67452a17440e553dd6fb6b919e9
gnanda1 <gnanda1987@gmail.com>
2020-06-23 11:24:57 -0400

    Update main.tf

--- 01_first_project/main.tf
+++ 01_first_project/main.tf
@@ -1,4 +1,4 @@
-# Create a new instance of the latest Ubuntu 14.04 on an
+# Create a new EC2 instance on an
 # t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
   region = "us-east-1"
