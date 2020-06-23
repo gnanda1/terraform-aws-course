d308137bcf54de9f9220756bb1bf410745a3f13f
Ganesh Na <gnanda1987@gmail.com>
2020-06-23 08:23:55 -0700

    First Terraform Project.

--- 01_first_project/main.tf
+++ 01_first_project/main.tf
@@ -1,9 +1,14 @@
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
   region = "us-east-1"
 }
 
-# create EC2 resource
-resource "aws_instance" "my_webserver" {
-  ami           = "ami-2757f631"
+resource "aws_instance" "web" {
+  ami           = "ami-085925f297f89fce1"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
